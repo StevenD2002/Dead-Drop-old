314e01e89aa25658537d49e8f67c5c4480927658
Steven Dew <stevendew02@gmail.com>
2022-01-29 12:11:14 -0600

    📦  FEAT: init commit

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -8,7 +8,7 @@ import React from "react"
 import { useColorScheme } from "react-native"
 import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { WelcomeScreen, DemoScreen, DemoListScreen } from "../screens"
+import { WelcomeScreen, DemoScreen, DemoListScreen, ChatScreen } from "../screens"
 import { navigationRef, useBackButtonHandler } from "./navigation-utilities"
 
 /**
@@ -27,6 +27,7 @@ export type NavigatorParamList = {
   welcome: undefined
   demo: undefined
   demoList: undefined
+  chat: undefined
 }
 
 // Documentation: https://reactnavigation.org/docs/stack-navigator/
@@ -38,11 +39,9 @@ const AppStack = () => {
       screenOptions={{
         headerShown: false,
       }}
-      initialRouteName="welcome"
+      initialRouteName="chat"
     >
-      <Stack.Screen name="welcome" component={WelcomeScreen} />
-      <Stack.Screen name="demo" component={DemoScreen} />
-      <Stack.Screen name="demoList" component={DemoListScreen} />
+      <Stack.Screen name="chat" component={ChatScreen} />
     </Stack.Navigator>
   )
 }
---
+++ app/screens/chat/chat-screen.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useReducer, useState } from "react"
+import { observer } from "mobx-react-lite"
+import { Button, TextInput, ViewStyle } from "react-native"
+import { Screen, Text } from "../../components"
+import Gun from "gun"
+// import { useNavigation } from "@react-navigation/native"
+// import { useStores } from "../../models"
+import { color } from "../../theme"
+import uuid from "react-native-uuid"
+import { useTheme } from "@react-navigation/native"
+
+const ROOT: ViewStyle = {
+  flex: 1,
+}
+const gun = Gun({ peers: ["http://drop.amii.moe:8765/gun"] })
+const initialState = {
+  messages: [],
+}
+
+function reducer(state, message) {
+  return {
+    messages: [...state.messages, message],
+  }
+}
+
+const INPUT = {
+  flexDirection: "row",
+}
+export const ChatScreen = observer(function ChatScreen() {
+  // Pull in one of our MST stores
+  // const { someStore, anotherStore } = useStores()
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const { theme } = useTheme()
+  useEffect(() => {
+    const messages = gun.get("messages")
+    messages.map().once((m) => {
+      dispatch({
+        name: m?.name,
+        message: m?.message,
+        createdAt: m?.createdAt,
+        key: m?.key,
+      })
+    })
+  }, [])
+
+  const [formState, setFormState] = useState({
+    name: "",
+    message: "",
+  })
+
+  function onChangeName(e) {
+    setFormState({
+      ...formState,
+      name: e,
+    })
+  }
+  function onChangeMessage(e) {
+    setFormState({
+      ...formState,
+      message: e,
+    })
+  }
+  const timeStamp = new Date().toISOString()
+  function saveMessage() {
+    const messages = gun.get("messages")
+    messages.set({
+      name: formState.name,
+      message: formState.message,
+      createdAt: timeStamp,
+      key: uuid.v4(),
+    })
+    setFormState({
+      name: "",
+      message: "",
+    })
+  }
+  // Pull in navigation via hook
+  // const navigation = useNavigation()
+  return (
+    <Screen style={ROOT} preset="scroll">
+      <>
+        {state.messages.map((message) => (
+          <Text key={message.key}>
+            {message.name}: {message.message}
+          </Text>
+        ))}
+      </>
+
+      <TextInput onChangeText={onChangeName} placeholder={`Name`} value={formState.name} />
+      <TextInput onChangeText={onChangeMessage} placeholder={`Message`} value={formState.message} />
+      <Button onPress={saveMessage} title="send message"></Button>
+    </Screen>
+  )
+})
